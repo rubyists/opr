be4ba0f06827fd57e697d3083f8a6546e90a1176
Tj (bougyman) Vanderpoel <tj@rubyists.com>
2019-12-16 13:24:43 -0600

    Initial commit

---
+++ lib/rubyists::opr.rb
@@ -0,0 +1,8 @@
+require "rubyists::opr/version"
+
+module Rubyists
+  module Opr
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rubyists::opr/version.rb
@@ -0,0 +1,5 @@
+module Rubyists
+  module Opr
+    VERSION = "0.1.0"
+  end
+end
